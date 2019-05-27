8b97d52db742d73739402b88624235017fc39d1e
Felipe Gasper <felipe@felipegasper.com>
2019-05-27 09:53:33 -0400

    POD

--- lib/Linux/PacketFilter.pm
+++ lib/Linux/PacketFilter.pm
@@ -5,6 +5,53 @@ use warnings;
 
 =encoding utf-8
 
+=head1 NAME
+
+Linux::PacketFilter - Simple interface to Linux packet filtering
+
+=head1 SYNOPSIS
+
+    # Reject any packet that starts with a period:
+    my $filter = Linux::PacketFilter->new(
+
+        # Load the accumulator with the 1st byte in the packet:
+        [ 'ld b abs', 0 ],
+
+        # If the accumulator value is an ASCII period, continue;
+        # otherwise, skip a line.
+        [ 'jmp jeq', ord('.'), 0, 1 ],
+
+        # If we continued, we'll get here and thus blackhole the packet.
+        [ ret => 0 ],
+
+        # If we get here, we skipped a line above, so we'll return
+        # the full packet.
+        [ ret => 0xffffffff ],
+    );
+
+=head1 DESCRIPTION
+
+This module is a simple, small, pure-Perl compiler for Linux's
+Berkeley Packet Filter (BPF) implementation.
+
+=head1 HOW DO I USE THIS?
+
+The best source I have found for learning about BPF itself is the BSD man
+pages; in bpf(4) see the section entitled B<FILTER MACHINE>.
+
+Linux-specific implementation notes are available in the kernel
+source tree at L</Documentation/networking/filter.txt|https://www.kernel.org/doc/Documentation/networking/filter.txt>. This contains a lot of detail
+about uses for BPF that don't pertain to packet filtering, though.
+
+L<Here is another helpful guide.|https://web.archive.org/web/20130125231050/http://netsplit.com/2011/02/09/the-proc-connector-and-socket-filters/> Take
+especial note of the need to convert between network and host byte order.
+
+Once you see what's going on with BPF in general, the SYNOPSIS above will
+make much more sense. Read on for further details about this particular
+implementation.
+
+=cut
+
 our %BPF;
 
 BEGIN {
@@ -53,35 +100,85 @@ BEGIN {
     }
 }
 
+=head1 METHODS
+
+=head2 $obj = I<CLASS>->new( @filters )
+
+Creates an object that represents an array of instructions for
+the BPF filter machine. Each @filters member is an array reference
+that represents a single instruction. Each array reference has either
+2 or 4 members, which correspond with the BPF_STMT and BPF_JUMP macros,
+respectively.
+
+The first member of each array reference is a space-separated string of
+options, lower-cased and without the leading C<BPF_>. So where in C you
+would write:
+
+    BPF_LD | BPF_W | BPF_ABS
+
+... in this module you write:
+
+    'ld w abs'
+
+Note that, in Linux anyway, the C<ld>, C<w>, C<imm>, C<add>, C<ja>, and C<k>
+options are all 0, so strictly speaking, you do not need to give these;
+thus, you could write the above as just:
+
+    'abs'
+
+For clarity's sake, though, you should probably avoid being quite so terse.
+:) I find it reasonable to omit C<w> and C<k> but to include everything else
+(so C<'ld abs'> for the above).
+
+=cut
+
+use constant {
+    _INSTR_PACK => 'S CC L',
+    _ARRAY_PACK => 'S x![P] P',
+};
+
+use constant _INSTR_LEN => length( pack _INSTR_PACK() );
+
 sub new {
     my $class = shift;
 
-    my $buf = ("\0" x (8 * @_));
+    my $buf = ("\0" x (_INSTR_LEN() * @_));
 
     my $f = 0;
 
     for my $filter (@_) {
         my $code = 0;
         for my $piece ( split m<\s+>, $filter->[0] ) {
-            $piece =~ tr<A-Z><a-z>;
-
             $code |= ($BPF{$piece} // die "Unknown BPF: $piece");
         }
 
         substr(
-            $buf, $f, 8,
+            $buf, $f, _INSTR_LEN(),
             pack(
-                'S CC L',
+                _INSTR_PACK(),
                 $code,
                 (@$filter == 2) ? (0, 0, $filter->[1]) : @{$filter}[2, 3, 1],
             ),
         );
 
-        $f += 8;
+        $f += _INSTR_LEN();
     }
 
-    return bless [ pack('S x![P] P', 0 + @_, $buf), $buf ], $class;
+    return bless [ pack(_ARRAY_PACK(), 0 + @_, $buf), $buf ], $class;
 }
+
+=head2 I<OBJ>->attach( $SOCKET )
+
+Attaches the filter instructions to the socket.
+
+Note that this class purposely omits public access to the value that
+is given to the underlying C<setsockopt(2)> system call. This is because
+that value contains a pointer to a Perl string. That pointer is only valid
+during this object's lifetime, and bad stuff (e.g., segmentation faults)
+can happen when you start using pointers to strings that Perl has already
+deleted.
+
+=cut
 
 sub attach {
     my ($self, $socket) = @_;
@@ -92,6 +189,8 @@ sub attach {
     };
 
     setsockopt $socket, Socket::SOL_SOCKET(), Socket::SO_ATTACH_FILTER(), $self->[0] or die "attach filter: $!";
+
+    return;
 }
 
 1;
